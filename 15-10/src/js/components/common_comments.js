1076135a86fd7e3961187fd26473d9cd01fcefe6
tomystery <931892155@qq.com>
2018-03-20 12:00:07 +0800

    15-10移动端用户收藏功能开发

--- 15-10/src/js/components/common_comments.js
+++ 15-10/src/js/components/common_comments.js
@@ -13,7 +13,8 @@ import {
   Input,
   Button,
   CheckBox,
-  Modal
+  Modal,
+  notification //加入了notification
 } from 'antd';
 const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
@@ -48,44 +49,44 @@ class CommonComments extends React.Component {
     var myFetchOptions = {
       method: 'GET'
     };
-    this.props.form.getFieldsValue();
+    var formdata = this.props.form.getFieldsValue();
+   
     fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=comment&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey + "&commnet=" + formdata.remark, myFetchOptions).then(response => response.json()).then(json => {
       this.componentDidMount();
     })
   };
 
-  addUserCollection(){
-var myFetchOptions={
-  method:'GET'
-};
-fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=uc&userid="+localStorage.userid+"&uniquekey="+yhis.props.uniquekey,myFetchOptions)
-.then(response=>response.json())
-.then(json=>{
-  //收藏成功以后进行的提醒
-})  
-}
+  addUserCollection() {
+    var myFetchOptions = {
+      method: 'GET'
+    };
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=uc&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey, myFetchOptions)
+      .then(response => response.json())
+      .then(json => {
+        //收藏成功以后进行的提醒
+        notification['success']({ message: 'React提醒', desrciption: '收藏此文章成功！' });
+      });
+  };
 
 
   render() {
-    let {
-      getFieldProps
-    } = this.props.form;
-    const {
-      comments
-    } = this.state;
-    const commentList = comments.length ?
-      comments.map((comment, index) => (<Card key={index} title={comment.UserName} extra={<a href='#' > 发布于 {comment.datetime} </a>}> <p> {comment.Comments} </p > </Card>)) : '没有加载任何评论';
+  let {getFieldProps} = this.props.form;
+  const {comments} = this.state;
+    const commentList = comments.length 
+    ? comments.map((comment, index) => (<Card key={index} title={comment.UserName} extra={<a href='#' > 发布于 {comment.datetime} </a>}> <p> {comment.Comments} </p > </Card>)) : '没有加载任何评论';
 
 
-    return (<div className="comment" >
+    return (
+    <div className="comment">
       <Row>
-        <Col span={24} > {commentList}
-
+        <Col span={24}> 
+        {commentList}
           <Form onSubmit={this.handleSubmit.bind(this)} >
             <FormItem label='您的评论' >
               <Input type='textarea' placeholder='随便写' {...getFieldProps('remark', { initialValue: '' })} />
-            </FormItem > <Button type='primary' htmlType='submit'> 提交评论 </Button>
-            <Button type='primary' htmlType='submit' onClick={this.addUserCollection().bind(this)}> 点击收藏 </Button>
+            </FormItem >
+            <Button type='primary' htmlType='submit'> 提交评论 </Button>&nbsp;&nbsp;
+            <Button type='primary' htmlType='button' onClick={this.addUserCollection.bind(this)}> 点击收藏 </Button>
           </Form > </Col> </Row >
     </div>
 
